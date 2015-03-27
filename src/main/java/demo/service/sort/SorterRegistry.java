f7e117b1d04339d6ae521c9438d7ae886fb27cbe
polinkot <pk_work@mail.ru>
2015-03-27 12:19:32 +0300

    Refactoring

--- src/main/java/demo/service/sort/SorterRegistry.java
+++ src/main/java/demo/service/sort/SorterRegistry.java
@@ -1,10 +1,35 @@
 package demo.service.sort;
 
 import demo.domain.SortTypeKey;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Polina on 22.03.2015.
  */
-public interface SorterRegistry {
-    ArraySorter getSorter(SortTypeKey key);
+@Component
+public class SorterRegistry {
+    Map<SortTypeKey, ArraySorter> registry = new HashMap<>();
+
+    public ArraySorter getSorter(SortTypeKey key) {
+        if (!registry.containsKey(key)) {
+            switch (key) {
+                case BUBBLE_SORT:
+                    registry.put(key, new BubbleSorter());
+                    break;
+                case MERGE_SORT:
+                    registry.put(key, new MergeSorter());
+                    break;
+                case LSDRADIX_SORT:
+                    registry.put(key, new LsdRadixSorter());
+                    break;
+                default:
+                    throw new IllegalArgumentException("Illegal sort key: " + key);
+            }
+        }
+
+        return registry.get(key);
+    }
 }
